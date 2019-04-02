2ee2ac1fd207fd01ced0b4a8d0bac658fcd9d68a
David Burke <dwburkinator@gmail.com>
2019-04-02 00:01:10 -0400

    Added models

---
+++ Models/InvoiceModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BatemanCafeteria.Models
+{
+    public class InvoiceModel
+    {
+        public int InvoiceID { get; set; }
+        public String Customer_name { get; set;}
+        public String Customer_email { get; set; }
+        public String Customer_phone { get; set; }
+        public DateTime Order_date { get; set; }
+        public String Order_time { get; set; }
+        public Decimal Order_total { get; set; }
+        public Boolean Payment_status { get; set; }
+        public List<OrderItemModel> Order_items { get; set; }
+    }
+}
---
+++ Models/OrderItemModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BatemanCafeteria.Models
+{
+    public class OrderItemModel
+    {
+        public int Order_itemID { get; set; }
+        public int InvoiceID { get; set; }
+        public int ItemID { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public String Special_instructions { get; set; }
+    }
+}
